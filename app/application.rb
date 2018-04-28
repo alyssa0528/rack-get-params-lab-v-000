5664f419eb9faac9c3d435683e570321a6c879d9
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-28 13:14:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,11 +16,12 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      @@cart.each do |cart_item|
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else @@cart.each do |cart_item|
         resp.write "#{cart_item}\n"
       end
-    elsif @@cart.empty?
-      resp.write "Your cart is empty"
+    end 
     else
       resp.write "Path Not Found" #for neither /items nor /search, return "Path Not Found"
     end
