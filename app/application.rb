a5c22b5d6632cd79cb3944e63c353d8f942c4e3d
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-28 13:18:42 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -29,6 +29,7 @@ class Application
         resp.write "added #{added_item}"
       else
         resp.write "We don't have that item"
+      end 
     else
       resp.write "Path Not Found" #for neither /items nor /search, return "Path Not Found"
     end
